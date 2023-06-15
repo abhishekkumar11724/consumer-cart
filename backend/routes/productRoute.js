d871d9818b090c28678c87d93187edc9c451c8cb
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-06-15 14:15:53 +0530

    Product- getall, deleteOne, addOne ✅

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const { getAllProducts } = require('../controllers/productController');
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
-
 router.route("/products").get(getAllProducts);
 
+router.route("/products/new").post(createProduct);
 
+router.route("/products/:id").put(updateProduct).delete(deleteProduct);
 
+// router.route("/products/:id").delete(deleteProduct);
 
 module.exports = router;
