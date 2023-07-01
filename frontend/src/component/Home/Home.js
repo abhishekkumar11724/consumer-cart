67585cf74785747088580ac3c2b5ac220c57a960
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-07-01 07:43:53 +0530

    made progress on actions and reducers of redux store for product

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,18 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js";
-
-const product = {
-  name: "Blue Tshirt",
-  image: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
-  price: "Rs. 3000",
-  _id: "abhishek",
-};
+import MetaData from "../layout/MetaData";
+import { getProduct, clearErrors } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { loading, error, products, productCount } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
+
   return (
     <Fragment>
+      <MetaData title="CONSUMER-CART" />
       <div className="banner">
         <p>Welcome to Consumer-Cart</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -26,15 +32,7 @@ const Home = () => {
       <h2 className="homeHeading">Featured Products</h2>
 
       <div className="container" id="container">
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
+        {products && products.map((product) => <Product product={product} />)}
       </div>
     </Fragment>
   );
