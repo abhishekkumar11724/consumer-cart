ece76c605582a67a1be9759c8c6dcb478665b443
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-07-03 15:08:17 +0530

    added component to the product details page with css

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -5,6 +5,7 @@ import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
 import Loader from "../layout/Loader/Loader";
+import ReactStars from "react-rating-stars-component";
 
 const ProductDetails = () => {
   // const match = useMatch("/product/:id");
@@ -18,11 +19,20 @@ const ProductDetails = () => {
     dispatch(getProductDetails(id));
   }, [dispatch, id]);
 
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20, 0,1)",
+    activeColor: "tomato",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  };
+
   return (
     <Fragment>
       <div className="ProductDetails">
-        <div>
-          <Carousel>
+        <div style={{ width: "100%" }}>
+          <Carousel className="carousel">
             {product.image &&
               product.image.map((item, i) => (
                 <img
@@ -33,6 +43,37 @@ const ProductDetails = () => {
                 />
               ))}
           </Carousel>
+        </div>
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product #{product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <ReactStars {...options} />
+            <span>({product.numOfReviews} Reviews)</span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input value="1" type="number" />
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <p>
+              Status:
+              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+          <div className="detailsBlock-4">
+            Description: <p>{product.description}</p>
+          </div>
+          <button className="submitReview">Submit Review</button>
         </div>
       </div>
     </Fragment>
