15c04a28a0ce31cf33a17732cea583543512ca98
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-07-04 19:17:50 +0530

    added search page

--- frontend/src/component/Search.js
+++ frontend/src/component/Search.js
@@ -1,22 +1,28 @@
 import React, { Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Search.css";
 
 const Search = () => {
+  const navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
 
   const searchSubmitHandler = (e) => {
     e.preventDefault();
     if (keyword.trim()) {
-      //   history.pushState(`/products/${keyword}`);
+      navigate(`/products/${keyword}`);
+    } else {
+      navigate("/products");
     }
   };
   return (
     <Fragment>
-      <form action="seachBox" onSubmit={searchSubmitHandler}>
+      <form className="searchBox" onSubmit={searchSubmitHandler}>
         <input
           type="text"
           placeholder="Search a Product ..."
           onChange={(e) => setKeyword(e.target.value)}
         />
+        <input type="submit" value="Search" />
       </form>
     </Fragment>
   );
