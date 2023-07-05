c8de5da10c1f394f5f7436ef2266610a4774c231
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-07-06 04:26:18 +0530

    made action and reducer for user login

---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,31 @@
+import {
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  CLEAR_ERRORS,
+} from "../constants/userConstants";
+
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const config = { headers: { "content-type": "application/json" } };
+
+    const { data } = await axios.post(
+      `/api/v1/login`,
+      { email, password },
+      config
+    );
+
+    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
+// Clearing Errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
